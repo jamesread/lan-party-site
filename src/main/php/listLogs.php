5a3303d7d129d9d5001f4cfbc0ca56aef55ba3b9
jamesread <contact@jwread.com>
2015-02-18 19:05:27 +0000

    Added support for displaying logs with associated metadata.

--- src/main/php/listLogs.php
+++ src/main/php/listLogs.php
@@ -5,10 +5,16 @@ require_once 'includes/widgets/header.php';
 
 requirePrivOrRedirect('LIST_LOGS');
 
-$sql = 'SELECT l.id, u.id AS user_id, u.username, l.date, l.message, l.ipAddress, g.css AS userGroupCss FROM log l LEFT JOIN users u on l.user = u.id JOIN `groups` g ON u.`group` = g.id ORDER BY id DESC LIMIT 50';
+$sql = 'SELECT l.id, u.id AS user_id, u.username, l.date, l.message, l.ipAddress, g.css AS userGroupCss, l.associatedUser, au.username AS associatedUsername, l.associatedEvent, ae.name AS associatedEventName FROM log l LEFT JOIN users u on l.user = u.id JOIN `groups` g ON u.`group` = g.id LEFT JOIN users au ON l.associatedUser = au.id LEFT JOIN events ae ON l.associatedEvent = ae.id ORDER BY id DESC LIMIT 50';
 $result = $db->query($sql);
 
 $logs = $result->fetchAll();
+
+for ($i = 0; $i < sizeof($logs); $i++) {
+	$logs[$i]['message'] = str_replace('_u_', '<a href = "profile.php?id=' . $logs[$i]['associatedUser'] . '">'. $logs[$i]['associatedUsername'] .'</a>', $logs[$i]['message']);
+	$logs[$i]['message'] = str_replace('_e_', '<a href = "viewEvent.php?id=' . $logs[$i]['associatedEvent'] . '">'. $logs[$i]['associatedEventName'] .'</a>', $logs[$i]['message']);
+}
+
 $tpl->assign('listLogs', $logs);
 $tpl->display('listLogs.tpl');
 
