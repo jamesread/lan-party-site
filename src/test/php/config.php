7d658a4e1a046ad51381127c95f40cd39f637f72
xconspirisist <xconspirisist@gmail.com>
2015-05-03 15:29:44 +0100

    Changed username and password for test user

--- src/test/php/config.php
+++ src/test/php/config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('CFG_DB_DSN', 'mysql:dbname=lps_test');
-define('CFG_DB_USER', 'lps_user');
-define('CFG_DB_PASS', 'password');
+define('CFG_DB_USER', 'lps_test');
+define('CFG_DB_PASS', 'lps_test');
 
 define('CFG_PASSWORD_SALT', '');
 
