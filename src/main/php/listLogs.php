edeefd7973ed37f9e18b11e72f8a1b177ac09aad
jamesread <contact@jwread.com>
2016-01-19 15:43:40 +0000

    Show more logs.

--- src/main/php/listLogs.php
+++ src/main/php/listLogs.php
@@ -5,7 +5,7 @@ require_once 'includes/widgets/header.php';
 
 requirePrivOrRedirect('LIST_LOGS');
 
-$sql = 'SELECT l.id, u.id AS user_id, u.username, l.date, l.message, l.ipAddress, g.css AS userGroupCss, l.associatedUser, au.username AS associatedUsername, l.associatedEvent, ae.name AS associatedEventName FROM log l LEFT JOIN users u on l.user = u.id JOIN `groups` g ON u.`group` = g.id LEFT JOIN users au ON l.associatedUser = au.id LEFT JOIN events ae ON l.associatedEvent = ae.id ORDER BY id DESC LIMIT 50';
+$sql = 'SELECT l.id, u.id AS user_id, u.username, l.date, l.message, l.ipAddress, g.css AS userGroupCss, l.associatedUser, au.username AS associatedUsername, l.associatedEvent, ae.name AS associatedEventName FROM log l LEFT JOIN users u on l.user = u.id JOIN `groups` g ON u.`group` = g.id LEFT JOIN users au ON l.associatedUser = au.id LEFT JOIN events ae ON l.associatedEvent = ae.id ORDER BY id DESC LIMIT 250';
 $result = $db->query($sql);
 
 $logs = $result->fetchAll();
